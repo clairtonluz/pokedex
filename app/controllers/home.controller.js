feffd83f02b70e033fb2d31ba76ceaddaa977d37
root <efraimgentil@gmail.com>
2016-06-25 12:29:35 -0300

    Home controller e Servico de busca do pokemon

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -1,4 +1,24 @@
 angular.module('pokedex')
-    .controller('HomeCtrl', ['$scope', function($scope) {
-        console.log('Home'); 
+    .controller('HomeCtrl', ['$scope' , 'BuscaPokemonService', function($scope , BuscaPokemonService) {
+
+      $scope.nome = "";
+      $scope.resultado = null;
+      $scope.pokemon = null;
+
+      $scope.buscar = function(){
+        $scope.pokemon = null;
+        BuscaPokemonService.buscaPokemon( $scope.nome , function(resultado){
+          if(resultado.length > 1){
+            $scope.resultado = resultado;
+          }else if(resultado.length == 1){
+            $scope.pokemon = resultado[0];
+          }else{
+            $scope.resultado = [];
+          }
+        });
+      }
+
+      $scope.selecionarPokemon = function(pokemon){
+        $scope.pokemon = pokemon;
+      }
     }]);
---
+++ app/services/BuscaPokemonService.js
@@ -0,0 +1,49 @@
+angular.module('pokedex').service("BuscaPokemonService" , ["$http" ,
+  function ($http) {
+    var self = this;
+    self.pokemonList = null;
+
+    function orderPokemonList(list){
+      self.pokemonList = list.sort(function(a, b ){
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+
+    function buscaPokemonsComNomeParecido(nome){
+      var result = [];
+      for(var i = 0 ; i < self.pokemonList.length ; i++ ){
+        var pokemon = self.pokemonList[i];
+        if( pokemon.name.includes( nome.toLowerCase() ) ){
+          if(!pokemon.pokeInfo){
+            carregarInformacoesPokemon(pokemon)
+          }
+          result.push(pokemon);
+        }
+      }
+      return result;
+    }
+
+    function carregarInformacoesPokemon( pokemon ){
+      $http.get("http://pokeapi.co/api/v2/pokemon/" + pokemon.name + "/").success(function(response){
+        pokemon.pokeInfo = response;
+      });
+    }
+
+    this.buscaPokemon = function(nome , callback){
+      if(this.pokemonList){
+        callback( buscaPokemonsComNomeParecido(nome) );
+      }else{
+        $http.get("http://pokeapi.co/api/v2/pokemon/?limit=811").success(function(response){
+          orderPokemonList( response.results );
+          callback( buscaPokemonsComNomeParecido(nome) );
+        });
+      }
+    }
+  }
+]);
