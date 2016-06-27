bafa5ab45f616d87ccbe208746297cfda21b1746
clairtonluz <clairton.c.l@gmail.com>
2016-06-27 20:23:00 -0300

    ...

--- app/controllers/evolution.controller.js
+++ app/controllers/evolution.controller.js
@@ -1,6 +1,7 @@
 angular.module('pokedex')
     .controller('EvolutionCtrl', ['$scope', '$routeParams', 'EvolucaoService',
         function ($scope, $routeParams, EvolucaoService) {
-            $scope.pokemon = EvolucaoService.buscaPokemon($routeParams.id);
+            var pokemon = EvolucaoService.buscaPokemon($routeParams.id);
+
         }
     ]);
