04bfbadbf90bfccb4334fb207e32b349c30d8e91
root <efraimgentil@gmail.com>
2016-06-27 21:53:25 -0300

    Tratamento erro ao consutlar habitate

--- app/controllers/habitats.controller.js
+++ app/controllers/habitats.controller.js
@@ -1,11 +1,13 @@
 angular.module("pokedex").controller("HabitatsController" , [ "HabitatsService" , "$scope" , function( HabitatsService , $scope  ) {
   var self = this;
 
-
   $scope.carregarHabitates = function(pokemonId){
-    console.log("WHAT ? " , pokemonId );
+    $scope.habitat = null;
+    $scope.errorNotFound = null;
     HabitatsService.carregaHabitates(pokemonId , function(data){
       $scope.habitat = data.habitat;
+    }, function error(data){
+      $scope.errorNotFound = "Não foi possível localizar o habitate do pokemon";
     });
   }
 
